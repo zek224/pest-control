5dd81da031007f9d5f91199938c26e470bec078d
Steven Kravitz <swk324@lehigh.edu>
2023-04-01 18:33:16 -0400

    Sign in Page rough draft

--- project/templates/header.js
+++ project/templates/header.js
@@ -4,7 +4,7 @@ template.innerHTML =
   <link rel="stylesheet" href="../static/header.css"/>
   <header>
     <nav>
-      <a href="home.html"id="header-logo"><img src="../static/images/vector.svg"></a>
+      <a href="home.html"id="header-logo"><img src="../images/vector.svg"></a>
       <a class="header-link" id="about" href="about.html">About</a>
       <a class="header-link" id="sign-up" href="signup.html">Sign up</a>
       <a class="header-link" id="sign-in" href="signin.html">Sign in</a>
