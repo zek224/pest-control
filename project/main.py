f88544ada6b005746bf3704ef3998804766513a1
zek224 <zek224@lehigh.edu>
2023-04-01 14:17:32 -0400

    fixed static folder

--- project/main.py
+++ project/main.py
@@ -1,11 +1,14 @@
 # main.py
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+app.mount('/static', StaticFiles(directory=os.path.join(current_dir, 'static')), name='static')
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
